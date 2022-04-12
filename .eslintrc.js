2b6f53ca621ba6c7d101e8dea54e3bc7cf839afb
Aliaksandr Sakouski <aliaksandr_sakouski@epam.com>
2022-04-13 00:23:13 +0300

    complete lesson 1 https://www.youtube.com/watch?v=bziVFvq8cLQ

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,18 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'react-app',
+    // 'prettier/react'
+  ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   plugins: ['prettier'],
   rules: {
-    'prettier/prettier': 'error',
+    'prettier/prettier': 'warn',
   },
 };
